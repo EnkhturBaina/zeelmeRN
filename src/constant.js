17932dab6748cb785f669b97db44290f4a5a9655
enkhturBaina <eegii.111.gg@gmail.com>
2022-05-02 19:19:25 +0800

    zaswar

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,4 @@
 export const test = "TES1T";
 export const COLOR_MAIN_GREEN = "#acd322";
+export const COLOR_MAIN_BLUE = "#3cc1fa";
 export const COLOR_MAIN_TEXT = "#4b5180";
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,43 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image } from "react-native";
-import { test } from "../constant";
-import { COLOR_MAIN_GREEN } from "../constant";
+import { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button as MainButton } from "react-native";
 import ZeelMeLogo from "../../assets/ZeelMeLogo.png";
+import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE } from "../constant";
+import { TextInput, Button } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
+
 const LoginScreen = () => {
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.mainView}>
-      <View style={{ width: "50%" }}>
-        <Image source={ZeelMeLogo} style={{ width: "100%" }} />
+      <View style={{ height: "30%", width: "80%", justifyContent: "center", alignItems: "center" }}>
+        <Image resizeMode="contain" source={ZeelMeLogo} style={{ width: "60%" }} />
       </View>
-      <Text style={{ color: COLOR_MAIN_GREEN }}>LoginScreen {test}</Text>
+      <View style={{ width: "60%" }}>
+        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Утасны дугаар" value={phone} onChangeText={(phone) => setText(phone)} />
+        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Нууц үг" value={password} onChangeText={(password) => setText(password)} />
+
+        <LinearGradient
+          start={{ x: 0.2, y: 0.8 }}
+          end={{ x: 1, y: 1.0 }}
+          style={{ width: "100%", marginTop: 20, alignItems: "center", padding: 5, borderRadius: 10 }}
+          // Button Linear Gradient
+          colors={[COLOR_MAIN_GREEN, COLOR_MAIN_BLUE]}
+        >
+          <TouchableOpacity title="Нэвтрэх" color="transparent" onPress={() => console.log("Pressed")}>
+            <Text style={{ color: "#fff" }}>Нэвтрэх</Text>
+          </TouchableOpacity>
+        </LinearGradient>
+        <View style={{ paddingTop: 20, flexDirection: "row", justifyContent: "space-between" }}>
+          <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <Text style={{ color: "black" }}>Бүртгүүлэх</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <Text style={{ color: "black" }}>Нууц үг сэргээх</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
@@ -17,9 +46,14 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
   mainView: {
+    backgroundColor: "white",
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+  },
+  generalInput: {
+    height: 40,
+    backgroundColor: "white",
   },
 });

9640b79bfe72c020a6c43a799d3cf5f972fc52cf
enkhturBaina <eegii.111.gg@gmail.com>
2022-05-03 13:02:50 +0800

    zaswar

--- src/constant.js
+++ src/constant.js
@@ -2,3 +2,5 @@ export const test = "TES1T";
 export const COLOR_MAIN_GREEN = "#acd322";
 export const COLOR_MAIN_BLUE = "#3cc1fa";
 export const COLOR_MAIN_TEXT = "#4b5180";
+
+export const LINEAR_BTN_STYLE = { justifyContent: "center", height: 40, marginTop: 20, alignItems: "center", padding: 5, borderRadius: 10 };
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity, Button as MainButton } from "react-native";
 import ZeelMeLogo from "../../assets/ZeelMeLogo.png";
-import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE } from "../constant";
+import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE, LINEAR_BTN_STYLE } from "../constant";
 import { TextInput, Button } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -11,25 +11,25 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.mainView}>
-      <View style={{ height: "30%", width: "80%", justifyContent: "center", alignItems: "center" }}>
-        <Image resizeMode="contain" source={ZeelMeLogo} style={{ width: "60%" }} />
+      <View style={styles.logoContainer}>
+        <Image resizeMode="contain" source={ZeelMeLogo} style={styles.logo} />
       </View>
-      <View style={{ width: "60%" }}>
-        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Утасны дугаар" value={phone} onChangeText={(phone) => setText(phone)} />
-        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Нууц үг" value={password} onChangeText={(password) => setText(password)} />
+      <View style={styles.inputContainer}>
+        <TextInput keyboardType="numeric" style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Утасны дугаар" value={phone} onChangeText={(phone) => setPhone(phone)} />
+        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Нууц үг" value={password} onChangeText={(password) => setPassword(password)} />
 
-        <LinearGradient
-          start={{ x: 0.2, y: 0.8 }}
-          end={{ x: 1, y: 1.0 }}
-          style={{ width: "100%", marginTop: 20, alignItems: "center", padding: 5, borderRadius: 10 }}
-          // Button Linear Gradient
-          colors={[COLOR_MAIN_GREEN, COLOR_MAIN_BLUE]}
-        >
-          <TouchableOpacity title="Нэвтрэх" color="transparent" onPress={() => console.log("Pressed")}>
+        <TouchableOpacity style={styles.touchableBtn} title="Нэвтрэх" color="transparent" onPress={() => console.log("Pressed" + phone + "---" + password)}>
+          <LinearGradient
+            start={{ x: 0.2, y: 0.8 }}
+            end={{ x: 1, y: 1.0 }}
+            style={LINEAR_BTN_STYLE}
+            // Button Linear Gradient
+            colors={[COLOR_MAIN_GREEN, COLOR_MAIN_BLUE]}
+          >
             <Text style={{ color: "#fff" }}>Нэвтрэх</Text>
-          </TouchableOpacity>
-        </LinearGradient>
-        <View style={{ paddingTop: 20, flexDirection: "row", justifyContent: "space-between" }}>
+          </LinearGradient>
+        </TouchableOpacity>
+        <View style={styles.registerResetView}>
           <TouchableOpacity onPress={() => console.log("Pressed")}>
             <Text style={{ color: "black" }}>Бүртгүүлэх</Text>
           </TouchableOpacity>
@@ -52,8 +52,24 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  logoContainer: {
+    flex: 1,
+    height: "30%",
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: "60%",
+  },
+  inputContainer: {
+    flex: 2,
+    width: "60%",
+  },
   generalInput: {
     height: 40,
     backgroundColor: "white",
   },
+  touchableBtn: {},
+  registerResetView: { paddingTop: 20, flexDirection: "row", justifyContent: "space-between" },
 });

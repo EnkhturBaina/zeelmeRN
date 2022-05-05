91243a26359de6b49c1cec0c818d25abd45efbde
enkhturBaina <eegii.111.gg@gmail.com>
2022-05-05 12:38:55 +0800

    zaswar

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity, Button as MainButton } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button as MainButton, ScrollView } from "react-native";
 import ZeelMeLogo from "../../assets/ZeelMeLogo.png";
-import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE, LINEAR_BTN_STYLE, COLOR_MAIN_TEXT } from "../constant";
-import { TextInput, Button } from "react-native-paper";
+import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE, LINEAR_BTN_STYLE, COLOR_MAIN_TEXT, REG_CHARS } from "../constant";
+import { TextInput, Button, Modal, Portal, Provider } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 
 const SignupScreen = () => {
@@ -10,46 +10,80 @@ const SignupScreen = () => {
   const [regSecondChar, setRegSecondChar] = useState("A");
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
-  return (
-    <View style={styles.mainView}>
-      <View style={styles.logoContainer}>
-        <Image resizeMode="contain" source={ZeelMeLogo} style={styles.logo} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.headerText}>Утасны дугаар нь таны нэвтрэх нэр болно</Text>
-        <View style={{ flexDirection: "row", width: "100%", alignItems: "center", marginBottom: 20 }}>
-          <Button
-            mode="outlined"
-            style={{
-              height: 40,
-              width: 20,
-              backgroundColor: "white",
-            }}
-            onPress={() => console.log("Pressed")}
-          >
-            {regFirstChar}
-          </Button>
-          <Button mode="outlined" style={styles.generalInput}>
-            {regSecondChar}
-          </Button>
-          <TextInput keyboardType="numeric" style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="flat" value={phone} onChangeText={(phone) => setPhone(phone)} />
-        </View>
-        <TextInput keyboardType="numeric" style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Утасны дугаар" value={phone} onChangeText={(phone) => setPhone(phone)} />
-        <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Нууц үг" value={password} onChangeText={(password) => setPassword(password)} />
+  const [whichRegChar, setWhichRegChar] = useState("");
 
-        <TouchableOpacity style={styles.touchableBtn} title="Нэвтрэх" color="transparent" onPress={() => console.log("Pressed" + phone + "---" + password)}>
-          <LinearGradient
-            start={{ x: 0.2, y: 0.8 }}
-            end={{ x: 1, y: 1.0 }}
-            style={LINEAR_BTN_STYLE}
-            // Button Linear Gradient
-            colors={[COLOR_MAIN_GREEN, COLOR_MAIN_BLUE]}
-          >
-            <Text style={{ color: "#fff" }}>Бүртгүүлэх</Text>
-          </LinearGradient>
-        </TouchableOpacity>
+  const [visible, setVisible] = useState(false);
+
+  const showModal = (value) => {
+    setWhichRegChar(value);
+    setVisible(true);
+  };
+  const hideModal = () => setVisible(false);
+
+  const setRegChar = (el, whichRegChar) => {
+    if (whichRegChar === "first") {
+      setRegFirstChar(el);
+    } else {
+      setRegSecondChar(el);
+    }
+    hideModal();
+  };
+  const containerStyle = {
+    backgroundColor: "white",
+    padding: 20,
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    flexWrap: "wrap",
+  };
+
+  return (
+    <Provider>
+      <View style={styles.mainView}>
+        <View style={styles.logoContainer}>
+          <Image resizeMode="contain" source={ZeelMeLogo} style={styles.logo} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.headerText}>Утасны дугаар нь таны нэвтрэх нэр болно</Text>
+          <View style={{ flexDirection: "row", width: "100%", alignItems: "center", marginBottom: 20 }}>
+            <Portal>
+              <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                {REG_CHARS.map((el, index) => {
+                  return (
+                    <Button key={index} style={{ margin: 3 }} onPress={() => setRegChar(el, whichRegChar)}>
+                      {el}
+                    </Button>
+                  );
+                })}
+              </Modal>
+            </Portal>
+            <View style={{ flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
+              <TouchableOpacity style={{ width: "20%", justifyContent: "center" }} onPress={() => showModal("first")}>
+                <Text>{regFirstChar}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{ width: "20%", justifyContent: "center" }} onPress={() => showModal("second")}>
+                <Text>{regSecondChar}</Text>
+              </TouchableOpacity>
+              <TextInput keyboardType="numeric" style={[styles.generalInput, { width: "60%" }]} activeOutlineColor={COLOR_MAIN_GREEN} mode="flat" value={phone} onChangeText={(phone) => setPhone(phone)} />
+            </View>
+          </View>
+          <TextInput keyboardType="numeric" style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Утасны дугаар" value={phone} onChangeText={(phone) => setPhone(phone)} />
+          <TextInput style={styles.generalInput} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Нууц үг" value={password} onChangeText={(password) => setPassword(password)} />
+
+          <TouchableOpacity style={styles.touchableBtn} title="Нэвтрэх" color="transparent" onPress={() => console.log("Pressed" + phone + "---" + password)}>
+            <LinearGradient
+              start={{ x: 0.2, y: 0.8 }}
+              end={{ x: 1, y: 1.0 }}
+              style={LINEAR_BTN_STYLE}
+              // Button Linear Gradient
+              colors={[COLOR_MAIN_GREEN, COLOR_MAIN_BLUE]}
+            >
+              <Text style={{ color: "#fff" }}>Бүртгүүлэх</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </Provider>
   );
 };
 
@@ -64,7 +98,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   logoContainer: {
-    flex: 1,
     height: "30%",
     width: "80%",
     justifyContent: "center",
@@ -79,9 +112,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textTransform: "uppercase",
     fontSize: 12,
+    marginBottom: 20,
   },
   inputContainer: {
-    flex: 2,
+    flex: 1,
     width: "60%",
   },
   generalInput: {

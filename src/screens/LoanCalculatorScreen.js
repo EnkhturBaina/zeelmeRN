c12d6ac3e9c684554340f7b560d78c7725dc7929
enkhturBaina <eegii.111.gg@gmail.com>
2022-06-13 13:44:53 +0800

    calc

--- src/screens/LoanCalculatorScreen.js
+++ src/screens/LoanCalculatorScreen.js
@@ -3,6 +3,7 @@ import { TextInput } from "react-native-paper";
 import { COLOR_MAIN_GREEN, COLOR_MAIN_BLUE, LINEAR_BTN_STYLE, COLOR_MAIN_TEXT, REG_CHARS } from "../constant";
 import { useState } from "react";
 import Slider from "@react-native-community/slider";
+import CurrencyInput from "react-native-currency-input";
 
 const LoanCalculatorScreen = () => {
   const [loanAmount, setLoanAmount] = useState("");
@@ -10,6 +11,7 @@ const LoanCalculatorScreen = () => {
   const [loanFee, setLoanFee] = useState("");
   const [loanPayment, setLoanPayment] = useState("");
 
+  const [value, setValue] = useState(0);
   const PMT = (ir, np, pv, fv, type) => {
     /*
      * ir   - interest rate per month
@@ -60,11 +62,23 @@ const LoanCalculatorScreen = () => {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <Text>LoanCalculatorScreen</Text>
-      <TextInput keyboardType="number-pad" returnKeyType={"done"} maxLength={12} style={{ marginBottom: 10 }} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Зээлийн хэмжээ" value={loanAmount} onChangeText={(loanAmount) => changeLoanAmount(loanAmount)} />
+      {/* <TextInput keyboardType="number-pad" returnKeyType={"done"} maxLength={12} style={{ marginBottom: 10 }} activeOutlineColor={COLOR_MAIN_GREEN} mode="outlined" label="Зээлийн хэмжээ" value={loanAmount} onChangeText={(loanAmount) => changeLoanAmount(loanAmount)} /> */}
+      {/* <CurrencyInput onChangeValue={(loanAmount) => changeLoanAmount(loanAmount)} value={loanAmount} prefix="" delimiter="," separator="." precision={0} onChangeText={(loanAmount) => changeLoanAmount(loanAmount)} /> */}
       <Text>Зээлийн хугацаа : {loanMonth}</Text>
-      {/* <Slider style={{ width: "100%", height: 40 }} value={loanMonth} minimumValue={0} maximumValue={60} thumbTintColor={COLOR_MAIN_GREEN} minimumTrackTintColor="#FFFFFF" step={12} maximumTrackTintColor="#000000" onValueChange={(loanMonth) => calcLoanPayment(loanMonth, loanFee)} />
+      <CurrencyInput
+        value={loanAmount}
+        onChangeValue={(formattedValue) => {
+          changeLoanAmount(formattedValue);
+          console.log(formattedValue);
+        }}
+        prefix=""
+        delimiter=","
+        separator="."
+        precision={0}
+      />
+      <Slider style={{ width: "100%", height: 40 }} value={loanMonth} minimumValue={0} maximumValue={60} thumbTintColor={COLOR_MAIN_GREEN} minimumTrackTintColor="#FFFFFF" step={12} maximumTrackTintColor="#000000" onValueChange={(loanMonth) => calcLoanPayment(loanMonth, loanFee)} />
       <Text>Зээлийн хүү : {loanFee}</Text>
-      <Slider style={{ width: "100%", height: 40 }} value={loanFee} minimumValue={0} maximumValue={3} thumbTintColor={COLOR_MAIN_GREEN} minimumTrackTintColor="#FFFFFF" step={0.5} maximumTrackTintColor="#000000" onValueChange={(loanFee) => calcLoanPayment(loanMonth, loanFee)} /> */}
+      <Slider style={{ width: "100%", height: 40 }} value={loanFee} minimumValue={0} maximumValue={3} thumbTintColor={COLOR_MAIN_GREEN} minimumTrackTintColor="#FFFFFF" step={0.5} maximumTrackTintColor="#000000" onValueChange={(loanFee) => calcLoanPayment(loanMonth, loanFee)} />
       <View style={styles.paymentContainer}>
         <Text>
           Сарын төлөлт: {loanPayment} --- {loanAmount}
